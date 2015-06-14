f53d0bf0b4e7864d49033896fb70a8bf26402a52
Joey B Capolongo <club4238@hotmail.com>
2015-06-14 13:40:50 -0400

    Issue #1: Create script to load the home page.

--- SeleniumTests/Program.cs
+++ SeleniumTests/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumTests
 {
@@ -10,6 +14,8 @@ namespace SeleniumTests
     {
         static void Main(string[] args)
         {
+            IWebDriver driver = new InternetExplorerDriver(@"C:\Users\Mrs Cappy\Downloads\IEDriverServer_Win32_2.45.0");
+            driver.Navigate().GoToUrl("granny-square-colors.com");
         }
     }
 }
